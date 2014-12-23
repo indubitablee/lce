1dfae53c910ea1f619823593634ca61a3acb0896
Grace Lee <grace.lee.9003@gmail.com>
2014-12-22 22:58:53 -0500

    working on basic layout

--- index.php
+++ index.php
@@ -1,1 +1,44 @@
-this is my new lce homepageasdfasdf
+<html>
+<head>
+	<!-- styles -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="site.css">
+	
+</head>
+
+<body>
+<div class="wrapper text-center">
+	<?php include('partials/nav.php');?>
+
+	<div class="col-md-12">
+		<div class="row">
+			<div class="col-md-6">
+				<img src="img/poster.jpg" style="display:inline-block;"/>
+			</div>
+			<div class="col-md-6">
+			</div>
+		</div>
+		asdfasdf
+	</div>
+
+
+
+
+
+
+
+	<div class="push"></div>
+</div>
+
+<div class="footer" style="background-color:#850000; color:white; width:100%; display:inline-block;">
+	<div class="col-md-12" style="padding-top:15px;">
+		&#169; 2014 DANJ Church Network | All Rights Reserved.
+	</div>
+</div>	
+
+
+<!-- scripts -->	
+<script src="https://code.jquery.com/jquery-1.11.2.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ partials/nav.php
@@ -0,0 +1,39 @@
+<div class="top-image">
+	<h1 style="color:white;">Love Changes Everything!<h1>
+</div>
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">LCE</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Page 1<span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Page 2</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Page 3 <span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">2013</a></li>
+            <li><a href="#">2012</a></li>
+            <li><a href="#">2011</a></li>
+            <li><a href="#">2010</a></li>
+            <li><a href="#">2009</a></li>
+          </ul>
+        </li>
+        <li><a href="#">Page 4</a></li>
+        <li><a href="#">Page 5</a></li>
+      </ul>
+
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
